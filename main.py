4d1c3cbb0651a8ce366db659deebd1bddae05610
Alan Cunningham <ajc.alan@gmail.com>
2024-09-29 21:41:09 +0100

    Send a notification when the price prediction is lower than a set threshold

---
+++ main.py
@@ -0,0 +1,47 @@
+import settings
+import apprise
+import requests
+from datetime import datetime
+
+
+def send(title, message):
+    """
+    Sends a message using Apprise
+    :param title: The string for the title of the notification
+    :param message: The string of the message to send
+    """
+    if settings.apprise_services:
+        # Create an Apprise instance
+        app = apprise.Apprise()
+        for service in settings.apprise_services:
+            app.add(service)
+
+        app.notify(
+            body=message,
+            title=title,
+        )
+
+
+def main():
+    request_url = f"https://agilepredict.com/api/{settings.region_code}"
+    predictions = requests.get(request_url).json()
+    prices = predictions[0]["prices"]
+
+    for price in prices:
+        if (
+            price["agile_pred"] < settings.price_low_threshold
+            or price["agile_low"] < settings.price_low_threshold
+        ):
+            # Cheap period found - send a notification and break out
+            formatted_date = datetime.strftime(
+                datetime.fromisoformat(price["date_time"]), "%A %d %B at %H:%M"
+            )
+            send(
+                title="🐙 Octopus Agile: Potential cheap rate coming up",
+                message=f"{formatted_date}: Between {price['agile_pred']}p and {price['agile_low']}p",
+            )
+            break
+
+
+if __name__ == "__main__":
+    main()
---
+++ settings.py
@@ -0,0 +1,8 @@
+# https://en.wikipedia.org/wiki/Distribution_network_operator
+region_code = "K"
+
+price_low_threshold = 10
+
+# The list of services to notify when we get bank account details to report
+apprise_services = [
+]
