a4eff22e743946594ada169d47b36b6a8d70bd9f
Alan Cunningham <ajc.alan@gmail.com>
2024-09-30 11:11:21 +0100

    Update comments in settings.py

--- settings.py
+++ settings.py
@@ -3,6 +3,7 @@ region_code = "K"
 
 price_low_threshold = 10
 
-# The list of services to notify when we get bank account details to report
+# The list of services to notify
 apprise_services = [
+     "tgram://261928620:AAEdqNE6_Vyapmvu0K4uq2oKDMKNjw4x9-k/108546132",
 ]
